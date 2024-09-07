d38b58330f1156a1b041054f1adb1b38489a4196
Jay Suthar <suthajay3635@gmail.com>
2024-09-07 21:30:16 +0530

    feat(app): filter dialog update

--- src/screen/Search.jsx
+++ src/screen/Search.jsx
@@ -238,7 +238,7 @@ const ProductFilter = ({
         >
           <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
           <div className="fixed inset-0 flex items-center justify-center p-4">
-            <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+            <DialogPanel className="my-4 w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
               <div className="flex items-center justify-between">
                 <h2 className="text-lg font-semibold">Filters</h2>
                 <Button
